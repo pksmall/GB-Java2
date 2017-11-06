081a816cb6f6509aa420261461e512ed526560b5
Pavel Korzhenko <pksmall@live.ru>
2017-11-06 06:54:31 -0400

    пересмотрена структура пакетов.

---
+++ src/hw1/pavelkorzhenko/obstances/Track.java
@@ -0,0 +1,21 @@
+package pavelkorzhenko.obstances;
+
+import animals.Animal;
+
+/**
+ * Write a description of class Track here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Track {
+    private int length;
+
+    public Track(int length) {
+        this.length = length;
+    }
+
+    public boolean doIt(Animal animal) {
+        return animal.run(length);
+    } 
+}
---
+++ src/hw1/pavelkorzhenko/obstances/Wall.java
@@ -0,0 +1,25 @@
+package pavelkorzhenko.obstances;
+
+import animals.Animal;
+import animals.Jumpable;
+
+/**
+ * Write a description of class Wall here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Wall {
+    private float height;
+
+    public Wall(float height) {
+        this.height = height;
+    }
+
+    public boolean doIt(Animal animal) {
+        if (animal instanceof Jumpable)
+            return ((Jumpable) animal).jump(height);
+        else
+            return false;
+    }
+}
